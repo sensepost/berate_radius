bef60535e0af52d0b16697086a1eb7883147202b
Szymon Ziolkowski <szymonziolkowski@Szymons-MacBook-Pro-2.local>
2023-06-29 11:31:31 +0200

    added args and help

--- berate_radius.sh
+++ berate_radius.sh
@@ -1,46 +1,58 @@
 #!/bin/sh
 
-PROGNAME="berate_radius"
+export LC_ALL=C
+
+PROGNAME="$(basename $0)"
 
 usage() {
-    echo "Usage: "$PROGNAME" [options] <wifi-interface> [<interface-with-internet>] [<access-point-name> [<passphrase>]]"
+    echo "Usage: "$PROGNAME" [-h] [--domain example.com] [--radius-password P@ssw0rd] [--radius-client-ip-range 0.0.0.0/0]"
     echo
     echo "Options:"
-    echo "  -h, --help              Show this help"
-    echo "  --version               Print version number"
-    echo "  -e <hosts_file>         DNS server will take into account additional hosts file"
+    echo "  -h, --help                               Show this help"
+    echo "  -d, --domain <domain>                    Domain of the certificate (default: \"example.com\")"
+    echo "  -p, --radius-password <password>         Radius client password (default: \"P@ssw0rd\")"
+    echo "  -r, --radius-client-ip-range <ip range>  Radius client allowed IP range (default: \"0.0.0.0/0\")"
     echo
-    echo "Useful informations:"
-    echo "  * If you're not using the --no-virt option, then you can create an AP with the same"
-    echo "    interface you are getting your Internet connection."
-    echo "  * You can pass your SSID and password through pipe or through arguments (see examples)."
-    echo "  * On bridge method if the <interface-with-internet> is not a bridge interface, then"
-    echo "    a bridge interface is created automatically."
-    echo
-    echo "Examples:"
-    echo "  "$PROGNAME" wlan0 eth0 MyAccessPoint MyPassPhrase"
-    echo "  echo -e 'MyAccessPoint\nMyPassPhrase' | "$PROGNAME" wlan0 eth0"
-    echo "  "$PROGNAME" wlan0 eth0 MyAccessPoint"
-    echo "  echo 'MyAccessPoint' | "$PROGNAME" wlan0 eth0"
-    echo "  "$PROGNAME" wlan0 wlan0 MyAccessPoint MyPassPhrase"
-    echo "  "$PROGNAME" -n wlan0 MyAccessPoint MyPassPhrase"
-    echo "  "$PROGNAME" -m bridge wlan0 eth0 MyAccessPoint MyPassPhrase"
-    echo "  "$PROGNAME" -m bridge wlan0 br0 MyAccessPoint MyPassPhrase"
-    echo "  "$PROGNAME" --driver rtl871xdrv wlan0 eth0 MyAccessPoint MyPassPhrase"
-    echo "  "$PROGNAME" --daemon wlan0 eth0 MyAccessPoint MyPassPhrase"
-    echo "  "$PROGNAME" --stop wlan0"
 }
 
 # defaults
-PASSWORD=P@ssw0rd
-RANGE=0.0.0.0/0
-DOMAIN=example.com
+export PASSWORD="P@ssw0rd"
+export RANGE="0.0.0.0/0"
+export DOMAIN="example.com"
 
-export DOMAIN=wifi.example.com
-/hostapd_configs/hostapd.conf.template > /hostapd_configs/hostapd.conf
+GETOPT_ARGS=$(getopt -o hd:p:r: -l "help","domain:","radius-password:","radius-client-ip-range:" -n "$PROGNAME" -- "$@")
+[ $? -ne 0 ] && exit 1
+eval set -- "$GETOPT_ARGS"
 
-export PASSWORD=P@ssw0rd 
-export RANGE=0.0.0.0/0 
+while :; do
+    case "$1" in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        -d|--domain)
+            shift
+            DOMAIN="$1"
+            shift
+            ;;
+        -p|--radius-password)
+            shift
+            PASSWORD="$1"
+            shift
+            ;;
+        -r|--radius-client-ip-range)
+            shift
+            RANGE="$1"
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+    esac
+done
+
+/hostapd_configs/hostapd.conf.template > /hostapd_configs/hostapd.conf 
 /hostapd_configs/hostapd.radius_client.template > /hostapd_configs/hostapd.radius_client
 
 
